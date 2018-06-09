8e332aaab0de24178cda9aa55ceaf4e24fefc05a
ByeongsuPark <byeongsu@soongsil.ac.kr>
2018-06-09 21:26:54 +0900

    Initial Commit

---
+++ app/src/main/java/byeongsu/soongsil/ac/kr/dogiyo/MainActivity.java
@@ -0,0 +1,13 @@
+package byeongsu.soongsil.ac.kr.dogiyo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
